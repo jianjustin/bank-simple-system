6f4d912f221464413500d2352edbfc905ba83ad8
jian <janine.jian.chen@gmail.com>
2018-12-14 13:36:25 +0800

    mysql sql init

--- resources/sql/sqlserver-bank-account.sql
+++ resources/sql/sqlserver-bank-account.sql
@@ -3,6 +3,7 @@ CREATE TABLE bankSystemDb.dbo.bank_account
     PK_ID BIGINT NOT NULL identity(1,1),
     BANK_ACCOUNT_CODE varchar(50) PRIMARY KEY NOT NULL,
     BANK_ACCOUNT_CARD varchar(200) NOT NULL,
+    BANK_ACCOUNT_PASSWORD varchar(200) NOT NULL,
     BANK_ACCOUNT_NAME varchar(200) DEFAULT '',
     BANK_ACCOUNT_ID_CARD varchar(200) DEFAULT '',
     BANK_ACCOUNT_MONEY varchar(50) DEFAULT 0.00,
@@ -14,6 +15,7 @@ EXEC sp_addextendedproperty 'MS_Description', '银行账户', 'SCHEMA', 'dbo', '
 EXEC sp_addextendedproperty 'MS_Description', '主键ID', 'SCHEMA', 'dbo', 'TABLE', 'bank_account', 'COLUMN', 'PK_ID'
 EXEC sp_addextendedproperty 'MS_Description', '账户编号(用于跟踪流水)', 'SCHEMA', 'dbo', 'TABLE', 'bank_account', 'COLUMN', 'BANK_ACCOUNT_CODE'
 EXEC sp_addextendedproperty 'MS_Description', '账户卡号', 'SCHEMA', 'dbo', 'TABLE', 'bank_account', 'COLUMN', 'BANK_ACCOUNT_CARD'
+EXEC sp_addextendedproperty 'MS_Description', '账户密码', 'SCHEMA', 'dbo', 'TABLE', 'bank_account', 'COLUMN', 'BANK_ACCOUNT_PASSWORD'
 EXEC sp_addextendedproperty 'MS_Description', '储户名称', 'SCHEMA', 'dbo', 'TABLE', 'bank_account', 'COLUMN', 'BANK_ACCOUNT_NAME'
 EXEC sp_addextendedproperty 'MS_Description', '身份证号码', 'SCHEMA', 'dbo', 'TABLE', 'bank_account', 'COLUMN', 'BANK_ACCOUNT_ID_CARD'
 EXEC sp_addextendedproperty 'MS_Description', '账户余额', 'SCHEMA', 'dbo', 'TABLE', 'bank_account', 'COLUMN', 'BANK_ACCOUNT_MONEY'
